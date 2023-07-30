ff874d928471eed3a47b8926a28b681d82403b78
ary <naimaryan1@yahoo.com>
2023-07-30 09:58:03 -0700

    RUN this file ./project.sh

---
+++ iot_thermocouple_client/project.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 
+# File:   project.sh.bash
+# Author: naima
+#
+# Created on Jul 30, 2023, 9:52:12 AM
+#
+chmod +x ./x86_thermocouple_client_main
+./x86_thermocouple_client_main 1 config_file.json
