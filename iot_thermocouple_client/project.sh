323150d8bb0d6469a8f3fc5976bbf1b7713a3199
ary <naimaryan1@yahoo.com>
2023-08-05 17:29:24 -0700

    updated

--- iot_thermocouple_client/project.sh
+++ iot_thermocouple_client/project.sh
@@ -5,5 +5,16 @@
 #
 # Created on Jul 30, 2023, 9:52:12 AM
 #
+#create necessary files 
+touch /var/log/heater
+chmod 666 /var/log/heater
+touch /var/log/status
+chmod 666 /var/log/status
+touch /var/log/messages
+chmod 666 /var/log/messages
+#start the thermocouple simulator
 chmod +x ./x86_thermocouple_client_main
 ./x86_thermocouple_client_main 1 config_file.json
+#start the client
+chmod +x ./tcsimd
+./tcsimd
